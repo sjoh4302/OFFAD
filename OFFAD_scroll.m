178b34874d90aa9cbcd69818f5b6bad43778d087
Christian Harding (laptop) <christian.harding@sjc.ox.ac.uk>
2021-05-19 00:04:01 +0100

    Added average LFP plot to scroller

--- OFFAD_scroll.m
+++ OFFAD_scroll.m
@@ -55,6 +55,20 @@ if isempty(OFFDATA.LFPpathin)==0
     LFPlength=size(LFPexampleObject,OFFDATA.ChannelsFullName(1),2);
 end
 
+
+%%%% Create dropdown menu to select horiztonal plot (Hypnogram or average LFP)
+uicontrol(g.Scroll,'Style', 'popupmenu','String',{'Hypnogram','Average LFP'},...
+    'FontSize',8,...
+    'Units','normalized',...
+    'BackgroundColor',[1 1 1],...
+    'Position',[0.02 0.95 0.08 0.04],...
+    'Tag','topPlotMenu',...
+    'UserData',0,...
+    'Callback',@changeTopPlot);
+
+
+
+
 %%% Generate hypnogram
 allEpoch=categorical(nan(ceil(length(OFFDATA.StartOP)/OFFDATA.PNEfs/4),1));
 allCol=repmat([0,0,0],length(allEpoch),1);
@@ -69,21 +83,28 @@ allCol(allEpoch=='W',1)=0; allCol(allEpoch=='W',2)=0.447; allCol(allEpoch=='W',3
 allCol(allEpoch=='R',2)=0.7;
 allCol(allEpoch=='N',3)=0;
 
-subplot('Position',[0.08 0.94 0.76 0.06]);
+subplot('Position',[0.12 0.84 0.72 0.14]);
 scatter(1:ceil(length(categorical(allEpoch))),categorical(allEpoch),10,allCol,'.')
-set(gca,'Tag','hypnogram')
+set(gca,'Tag','topPlot')
+set(gca,'NextPlot','Add')
 set(gca,'xtick',[])
 set(gca,'xcolor',[1,1,1])
 set(gca,'xlim',[0,ceil(length(OFFDATA.StartOP)/OFFDATA.PNEfs/OFFDATA.epochLen)])
 xline(gca,1,'LineWidth',2,'Color',allCol(1,:))
 
-uicontrol(g.Scroll,'Style', 'text','String',string(allEpoch(1)),...
-    'FontWeight','bold','FontSize',35,...
+%Change vigilance state names to full
+allEpochFull=allEpoch;
+allEpochFull(allEpoch=='W')='WAKE';
+allEpochFull(allEpoch=='N')='NREM';
+allEpochFull(allEpoch=='R')='REM';
+
+uicontrol(g.Scroll,'Style', 'text','String',string(allEpochFull(1)),...
+    'FontWeight','bold','FontSize',23,...
     'Units','normalized',...
     'ForegroundColor',allCol(1,:),... 
     'BackgroundColor',[1 1 1],...
     'Tag','VigState',...
-    'Position',[0.01 0.94 0.06 0.08]);  %Plot current state
+    'Position',[0.15 0.79 0.1 0.05]);  %Plot current state
 
 clear vigStateNames
 
@@ -129,13 +150,13 @@ end
 uicontrol(g.Scroll,'Style', 'pushbutton','String', '<',...
     'FontWeight','bold','FontSize',16,...
     'Units','normalized',...
-    'Position',[0.30 0.9 0.04 0.03],...
+    'Position',[0.30 0.8 0.04 0.03],...
     'Callback',@shiftScroll);
 
 uicontrol(g.Scroll,'Style', 'pushbutton','String', '>',...
     'FontWeight','bold','FontSize',16,...
     'Units','normalized',...
-    'Position',[0.58 0.9 0.04 0.03],...
+    'Position',[0.58 0.8 0.04 0.03],...
      'Callback',@shiftScroll);
 
 %Show X-scale
@@ -262,10 +283,10 @@ uicontrol(g.Scroll,'Style','radiobutton','String','All',...
     'Tag','RBmaster',...
     'UserData',length(OFFDATA.ChannelsFullName),...
     'Callback',@specificOpt,...
-    'Position',[0.03 0.89 0.05 0.03]);  
+    'Position',[0.03 0.79 0.05 0.03]);  
 
 %Create channel buttons
-radioHeight1=linspace(0.09,0.89,length(OFFDATA.ChannelsFullName)+1);
+radioHeight1=linspace(0.09,0.79,length(OFFDATA.ChannelsFullName)+1);
 radioHeight2=(radioHeight1(2)-radioHeight1(1))-(radioHeight1(2)-radioHeight1(1))/10;
 radioHeight2=radioHeight2-0.027;
 radioHeight1=flip(radioHeight1(1:end-1));
@@ -276,7 +297,7 @@ for m = 1:length(OFFDATA.ChannelsFullName)
     'BackgroundColor',[1 1 1],...
     'Value',rbVal(m),...
     'Enable',rbEnable,...
-    'UserData',OFFDATA.ChannelsFullName(m),...
+    'UserData',m,...
     'Tag',rbName,...
     'Callback',@changeOpt,...
     'Position',[0.045 radioHeight1(m)+radioHeight2*0.5+0.007 0.015 0.03])
@@ -291,6 +312,7 @@ uicontrol(g.Scroll,'Style', 'text','String',chanString,...
     'FontWeight','bold','FontSize',10,...
     'Units','normalized',...
     'Tag','optChan',...
+    'UserData',1,...
     'Position',[0.37 0.01 0.18 0.03])
 
 
@@ -301,14 +323,14 @@ uicontrol(g.Scroll,'Style', 'text','String',chanString,...
 uicontrol(g.Scroll,'Style', 'edit','String','0',...
     'FontWeight','bold','FontSize',10,...
     'Units','normalized',...
-    'Position',[0.42 0.9 0.08 0.03],'CreateFcn',@drawOFFP,...
+    'Position',[0.48 0.8 0.06 0.028],'CreateFcn',@drawOFFP,...
     'Tag','scrollTime',...
     'Callback',@drawOFFP);
-uicontrol(g.Scroll,'Style', 'text','String','Time(s)',...
+uicontrol(g.Scroll,'Style', 'text','String','Recording time(s)',...
     'FontSize',10,...
     'BackgroundColor',[1 1 1],...
     'Units','normalized',...
-    'Position',[0.375 0.9 0.045 0.03]);
+    'Position',[0.38 0.8 0.1 0.028]);
 
 
 
@@ -357,7 +379,7 @@ end
 
 
 numChan=length(OFFDATA.ChannelsFullName);
-subplot2=linspace(0.1,0.9,numChan+1);
+subplot2=linspace(0.1,0.8,numChan+1);
 subplot4=(subplot2(2)-subplot2(1))-(subplot2(2)-subplot2(1))/10;
 subplot2=flip(subplot2(1:end-1));
 for i = 1:numChan
@@ -512,13 +534,111 @@ catch
      cla
 end 
  
+
+%%%% Change top plot
+if get(findobj('Tag','topPlotMenu'),'UserData')==1
+axes(findobj('Tag','topPlot'))
+cla(gca)
+colorbar(gca,'off')
+ylabel(gca,'')
+
+%%% 1: Plot hypnogram
+if get(findobj('Tag','topPlotMenu'),'Value')==1
+scatter(1:ceil(length(categorical(allEpoch))),categorical(allEpoch),10,allCol,'.')
+set(gca,'xtick',[])
+set(gca,'xcolor',[1,1,1])
+set(gca,'xlim',[0,ceil(length(OFFDATA.StartOP)/OFFDATA.PNEfs/OFFDATA.epochLen)])
+xline(gca,1,'LineWidth',2,'Color',allCol(1,:))
+
+
+%%% 2: Plot average slow wave
+elseif  get(findobj('Tag','topPlotMenu'),'Value')==2
+swChan=get(findobj('Tag','histChan'),'Value'); %select channel to generate slow wave profile
+%%% Recalculate OFF start and end time
+adjOFFStarts=find(OFFDATA.StartOP(:,swChan));
+adjOFFEnds=find(OFFDATA.EndOP(:,swChan));
+
+%Remove short gaps
+chooseON=adjOFFStarts(2:end)-adjOFFEnds(1:end-1)-1<(OFFDATA.PNEfs/1000*str2num(get(findobj('Tag','maxInterVal'),'String')));
+chooseONstart=~[0;chooseON];
+chooseONend=~[chooseON;0];
+adjOFFStarts=adjOFFStarts(chooseONstart);
+adjOFFEnds=adjOFFEnds(chooseONend);
+
+%Remove short off-periods
+chooseOFF=adjOFFEnds-adjOFFStarts+1>(OFFDATA.PNEfs/1000*str2num(get(findobj('Tag','minDurVal'),'String')));
+adjOFFStarts=adjOFFStarts(chooseOFF);
+adjOFFEnds=adjOFFEnds(chooseOFF);
+
+
+%Remove off-periods at start/end of recordings
+minOFF=ceil(OFFDATA.PNEfs); maxOFF=length(OFFDATA.StartOP)-ceil(OFFDATA.PNEfs);
+fullOFFindex=adjOFFStarts>minOFF&adjOFFStarts<maxOFF;
+adjOFFStarts=adjOFFStarts(fullOFFindex);
+adjOFFEnds=adjOFFEnds(fullOFFindex);
+
+%Get adjusted off-period durations
+MSdurOP=(adjOFFEnds-adjOFFStarts+1)*(1/OFFDATA.PNEfs)*1000; %Off period length in MS
+LFPdurOP=ceil(((adjOFFEnds-adjOFFStarts+1)*OFFDATA.LFPfs)/OFFDATA.PNEfs)-1;  %Off period length in LFP data points
+clear adjOFFEnds
+
+numOFFPsamp=length(adjOFFStarts);
+dispRangeSec=1;%range of average LFP in seconds
+dispRangeMSec=dispRangeSec*1000;
+dispRangeLFP=(floor(OFFDATA.LFPfs*dispRangeSec)+1+mod(floor(OFFDATA.LFPfs*dispRangeSec),2));%Number of LFP data points to display per OFF period
+dispRangeLFPhalf=floor(dispRangeLFP/2);
+
+%Average OFF period LFPs in 10th percentiles
+[sortOFFlengths,sortOFFpos]=sort(MSdurOP);
+for k=1:10
+    currIndex=ceil(length(MSdurOP)*((k-1)/10))+1:ceil(length(MSdurOP)*(k/10));
+    avPercentileOFFlength(k)=mean(sortOFFlengths(currIndex));
+    centeredOFFperiods=nan(length(currIndex),dispRangeLFP);
+    %Extract OFF period LFP
+    for swSamp = 1:length(currIndex)
+        selectOP=sortOFFpos(currIndex(swSamp));
+        LFPstart=round((adjOFFStarts(selectOP)/OFFDATA.PNEfs)*OFFDATA.LFPfs);
+        centeredOFFperiods(swSamp,:)=[LFPtmp.(OFFDATA.ChannelsFullName(swChan))(LFPstart-dispRangeLFPhalf:LFPstart+dispRangeLFPhalf)];    
+    end
+    avePercentileOFF(:,k)=mean(centeredOFFperiods);
+    clear centeredOFFperiods
+end
+
+%Plot average LFPs
+cMap=flip(copper);
+lineCols=cMap(round(rescale(avPercentileOFFlength)*255)+1,:);%rescale lengths to colormap
+lineCols=flip(lineCols);
+avePercentileOFF=flip(avePercentileOFF,2);
+LFPtime=linspace(-dispRangeMSec/2,dispRangeMSec/2,dispRangeLFP)';
+for i=1:length(avPercentileOFFlength)
+    plot(LFPtime,avePercentileOFF(:,i),'Color',lineCols(i,:),'LineWidth',3)
+    hold on
+end
+set(gca,'XAxisLocation', 'top')
+set(gca,'xtick',[LFPtime(1):100:LFPtime(end)])
+set(gca,'xcolor',[0,0,0])
+xlim([LFPtime(1),LFPtime(end)])
+ylim([min(min((avePercentileOFF))),max(max((avePercentileOFF)))])
+ylabel(char(OFFDATA.LFPunit))
+colormap(cMap)
+c=colorbar('Ticks',0:0.2:1,...
+         'TickLabels',round(linspace(avPercentileOFFlength(1),avPercentileOFFlength(end),6)));
+c.Label.String = 'Mean duration (ms)';
+clear chooseON chooseONstart chooseONend adjOFFStarts avePercentileOFF avPercentileOFFlength centeredOFFperiods fullOFFindex
+end
+set(findobj('Tag','topPlotMenu'),'UserData',0);
+end
+
 %Plot current epoch vig state info
-set(findobj(get(findobj('Tag','hypnogram'),'Children'),'Type','ConstantLine'),...
-    'Value',(str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1)
-set(findobj(get(findobj('Tag','hypnogram'),'Children'),'Type','ConstantLine'),...
-    'Color',allCol((str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1,:))
-set(findobj('Tag','VigState'),'String',string(allEpoch((str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1)))
+set(findobj('Tag','VigState'),'String',string(allEpochFull((str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1)))
 set(findobj('Tag','VigState'),'ForegroundColor',allCol((str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1,:))
+if get(findobj('Tag','topPlotMenu'),'Value')==1 
+set(findobj(get(findobj('Tag','topPlot'),'Children'),'Type','ConstantLine'),...
+    'Value',(str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1)
+set(findobj(get(findobj('Tag','topPlot'),'Children'),'Type','ConstantLine'),...
+    'Color',allCol((str2num(get(findobj('Tag','scrollTime'),'String'))/OFFDATA.epochLen)+1,:))
+end
+
 
 end
 
@@ -536,6 +656,11 @@ elseif source.String=='>'
     set(findobj('Tag','scrollTime'),'String',num2str(newTime));
 end
 
+%Set toplot to replot if hypnogram
+if  get(findobj('Tag','topPlotMenu'),'Value')==1
+    set(findobj('Tag','topPlotMenu'),'UserData',1)
+end
+
 drawOFFP
 
 end
@@ -549,13 +674,17 @@ if get(findobj('Tag','RBmaster'),'Value')==1
     set(findobj('Tag','minDurVal'),'UserData',repmat(newVal,1,get(findobj('Tag','RBmaster'),'UserData'))); 
 else
     chanDurThresh=get(findobj('Tag','minDurVal'),'UserData');
-    currChan=double(string(regexp(get(findobj('Tag','optChan'),'String'),'\d','match')));
+    currChan=get(findobj('Tag','optChan'),'UserData');
     chanDurThresh(currChan)=newVal;
     set(findobj('Tag','minDurVal'),'UserData',chanDurThresh);
 end
 
 %Set histograms to replot
 set(findobj('Tag','OFFAD_SCROLL'),'UserData',1)
+%Set top plot to replot if average LFP
+if  get(findobj('Tag','topPlotMenu'),'Value')==2
+    set(findobj('Tag','topPlotMenu'),'UserData',1)
+end
 
 drawOFFP
 end
@@ -573,13 +702,17 @@ newVal=get(findobj('Tag','maxInter'),'Value');
 set(findobj('Tag','maxInterVal'),'String',num2str(newVal))
 %Set histograms to replot
 set(findobj('Tag','OFFAD_SCROLL'),'UserData',1)
+%Set toplot to replot if average LFP
+if  get(findobj('Tag','topPlotMenu'),'Value')==2
+    set(findobj('Tag','topPlotMenu'),'UserData',1)
+end
 
 %Set channel tresholds
 if get(findobj('Tag','RBmaster'),'Value')==1
     set(findobj('Tag','maxInterVal'),'UserData',repmat(newVal,1,get(findobj('Tag','RBmaster'),'UserData'))); 
 else
     chanInterThresh=get(findobj('Tag','maxInterVal'),'UserData');
-    currChan=double(string(regexp(get(findobj('Tag','optChan'),'String'),'\d','match')));
+    currChan=get(findobj('Tag','optChan'),'UserData');
     chanInterThresh(currChan)=newVal;
     set(findobj('Tag','maxInterVal'),'UserData',chanInterThresh);
 end
@@ -599,12 +732,13 @@ end
 function shiftChan(source,~)
 %Set histograms to replot
 set(findobj('Tag','OFFAD_SCROLL'),'UserData',1)
+%Set toplot to replot if average LFP
+if  get(findobj('Tag','topPlotMenu'),'Value')==2
+    set(findobj('Tag','topPlotMenu'),'UserData',1)
+end
 
 drawOFFP
 end
-
-
-
 
 
 
@@ -620,8 +754,9 @@ if source.Value == 0
             set(findobj('Tag',rbName),'Value',0)
         end
     end
-    set(findobj('Tag','optChan'),'String',get(findobj('Tag','RB1'),'UserData'))
-     
+    set(findobj('Tag','optChan'),'String',OFFDATA.ChannelsFullName(get(findobj('Tag','RB1'),'UserData')))
+    set(findobj('Tag','optChan'),'UserData',get(findobj('Tag','RB1'),'UserData'))
+    
     %Display correct channel histograms
     set(findobj('Tag','histChan'),'Value',1)
     shiftChan
@@ -645,6 +780,7 @@ elseif source.Value == 1
             set(findobj('Tag',rbName),'Value',1)
         end
         set(findobj('Tag','optChan'),'String','All channels')
+        set(findobj('Tag','optChan'),'UserData',0)
 
         %Reset optimization thresholds
         set(findobj('Tag','minDurVal'),'String',0)
@@ -676,22 +812,28 @@ end
 source.Value=1;
 
 %Display current channel being optimized
-set(findobj('Tag','optChan'),'String',source.UserData)
+set(findobj('Tag','optChan'),'String',OFFDATA.ChannelsFullName(source.UserData))
+set(findobj('Tag','optChan'),'UserData',source.UserData)
 
 %Display correct channel histograms
-set(findobj('Tag','histChan'),'Value',double(string(regexp(source.UserData,'\d','match'))))
+set(findobj('Tag','histChan'),'Value',source.UserData)
 
 %Display correct optimization thresholds
 allIntThresh=get(findobj('Tag','maxInterVal'),'UserData');
-set(findobj('Tag','maxInterVal'),'String',num2str(allIntThresh(double(string(regexp(source.UserData,'\d','match'))))))
+set(findobj('Tag','maxInterVal'),'String',num2str(allIntThresh(source.UserData)))
 allDurThresh=get(findobj('Tag','minDurVal'),'UserData');
-set(findobj('Tag','minDurVal'),'String',num2str(allDurThresh(double(string(regexp(source.UserData,'\d','match'))))))
+set(findobj('Tag','minDurVal'),'String',num2str(allDurThresh(source.UserData)))
 
 %Display new histograms
 shiftChan
 
 end
 
+function changeTopPlot(source,~)
+%Set top plot to replot
+set(findobj('Tag','topPlotMenu'),'UserData',1);
+drawOFFP
+end
 
 
 end
