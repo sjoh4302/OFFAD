c593ea3536a4d0f8104847cfc29dfbec6da28a9f
Christian Harding (laptop) <christian.harding@sjc.ox.ac.uk>
2022-09-15 00:41:49 +0100

    commented and changed order of statistic button

--- OFFAD_channelstats.m
+++ OFFAD_channelstats.m
@@ -12,22 +12,18 @@ function [OFFDATA]=OFFAD_channelstats(OFFDATA,plotType)
 %
 
 
-%Close loading window
-g.Cluster = findobj('tag', 'OFFAD_CLUSTER');
-close(g.Cluster)
-
-%Plotting
-g.Channelstats = figure('Units','points', ...
-... %	'Colormap','gray', ...
-	'PaperPosition',[18 180 576/2 432], ...
-	'PaperUnits','points', ...
-	'name', 'OFFAD (OFF_period Automated Detection) - Channel Statistics', ... 
+% Get screensize to set figure position
+screensize = get( groot, 'Screensize' ); 
+    
+% Create figure for scrolling window 
+g.Channelstats = figure('name', 'OFFAD (OFF_period Automated Detection) - Channel Statistics', ... 
 	'numbertitle', 'off', ...
-	'Position',[200 100 800 450], ...
+	'Position',[screensize(3)/6,screensize(4)/8,4*screensize(3)/6,screensize(4)/1.4], ...
     'Tag','OFFAD_CHANNELSTATS');
 drawnow 
 
-%% Decide if plotting original or adjusted OFF period data
+% Plotting original or adjusted (with miniumum duration/maxiumum interruption
+% thresholds) OFF period data
 if plotType==1;
     if isfield(OFFDATA,'StartOPadjusted')
         selectData='OPadjusted';  
@@ -38,7 +34,7 @@ else
     selectData='OP';
 end
 
-%%% Display plotting settings
+% Display current plotting selection
 if contains(selectData,'adjusted')
     displayName='Adjusted';
 else
@@ -49,7 +45,7 @@ uicontrol(g.Channelstats,'Style', 'text','String',displayName,...
     'Units','normalized',...
     'Position',[0.72 0.8 0.28 0.1])
     
-%%% Add switch data type button
+% Button to switch between original and adjusted data types
 if plotType==0
     switchName='Switch to adjusted';
     switchCallback='close(findobj(''Tag'',''OFFAD_CHANNELSTATS''));[OFFDATA]=OFFAD_channelstats(OFFDATA,1);';
@@ -63,7 +59,6 @@ else
     switchCallback='close(findobj(''Tag'',''OFFAD_CHANNELSTATS''));[OFFDATA]=OFFAD_channelstats(OFFDATA,0);';
     visibility='off';
 end
-
 uicontrol(g.Channelstats,'Style', 'pushbutton','String',switchName,...
 'FontSize',8,...
 'Visible',visibility,...
@@ -71,15 +66,14 @@ uicontrol(g.Channelstats,'Style', 'pushbutton','String',switchName,...
 'Callback',switchCallback,...
 'Position',[0.79 0.8 0.14 0.04])
 
-%% Compute statistics
-
-%%% Generate temporary variable
+% Generate temporary variable to establish PNE length
 exampleObject = matfile(OFFDATA.PNEpathin);
 PNElength=size(exampleObject,OFFDATA.ChannelsFullName(1),2);
 PNEtimeTemp=[1/OFFDATA.PNEfs:1/OFFDATA.PNEfs:PNElength/OFFDATA.PNEfs]';
 clear PNElength exampleObject
 
-%%%%%%% Off durations plot
+% Statistics 1: OFF period duration
+% Find duration of all OFF periods in recording for each channel (ms)
 OFFdurations=[];
 OFFdurationsID=[];
 for i = 1:length(OFFDATA.Channels)
@@ -92,9 +86,9 @@ for i = 1:length(OFFDATA.Channels)
    clear start_end
 end
 
-%%%%%%% Channel coherence plot 
-%Coherence between two channels is the average proportion of time both are
-%in same state.
+% Statistics 2: OFF period inter-channel coherence
+% Calculate average proportion of OFF periods temporally shared between 
+% each pair of channels
 for i = 1:length(OFFDATA.Channels)
     display(['Calculating channel ',num2str(OFFDATA.Channels(i))])
     for j = 1:length(OFFDATA.Channels)
@@ -102,41 +96,47 @@ for i = 1:length(OFFDATA.Channels)
            /sum(OFFDATA.(['All',selectData])(:,i))...
            +length(intersect(PNEtimeTemp(OFFDATA.(['All',selectData])(:,j)),PNEtimeTemp(OFFDATA.(['All',selectData])(:,i))))...
            /sum(OFFDATA.(['All',selectData])(:,j)))/2;
-     
-    end    
+     end    
 end
 coherenceMat(coherenceMat==1)=NaN;
 coherence=reshape(coherenceMat,[],1);
 coherenceID=reshape(repmat(OFFDATA.Channels,length(OFFDATA.Channels),1),[],1);
-%Store summary info
+% Store summary info
 OFFDATA.(['Stats',selectData]).MeanCoherence=nanmean(coherenceMat,2);
 clear coherenceMat
 
-%%%%%%%%% Off period number
+% Statistics 3: OFF period number
+% Find total number of OFF periods in recording for each channel 
 OFFDATA.(['Stats',selectData]).OPnumber=full(sum(OFFDATA.(['Start',selectData])))';
 
-%%%%%%%%% Off period occupancy time
+% Statistics 4: OFF period occupancy
+% Find total time of spent in OFF state in recording for each channel (hours)
 OFFDATA.(['Stats',selectData]).OPoccupancy_time=hours(seconds(sum(OFFDATA.(['All',selectData])/OFFDATA.PNEfs)))';
 
-%%%%%%%%% Optional LFP info
+% Statistics 5: LFP profile [OPTIONAL]
+% Find average amplitude of LFP associated with each OFF period per channel
 try
-    %Filter settings
+    % Filter LFP
     p1=0.5; p2=100; s1=0.1; s2=120;
     Wp=[p1 p2]/(OFFDATA.LFPfs/2); Ws=[s1 s2]/(OFFDATA.LFPfs/2); Rp=3; Rs=30;
     [n, Wn]=cheb2ord(Wp,Ws,Rp,Rs);
     [bb1,aa1]=cheby2(n,Rs,Wn);
+    
+    % Create empty variables to store LFP ampltiude
     LFPamp=[];
     LFPampID=[];
+    
     for i = 1:length(OFFDATA.Channels)
+         
          sig=load(OFFDATA.LFPpathin,OFFDATA.ChannelsFullName(i));
          sig=sig.(OFFDATA.ChannelsFullName(i));
          if OFFDATA.FiltLFP==1
              sig=filtfilt(bb1,aa1,double(sig));
              %DC offset
              sig = sig-mean(sig);
-       
          end
          
+         % Extract concommitant LFP during each MUA OFF period
          try
              LFPampTmp=single(sig(unique(round(mod(PNEtimeTemp(OFFDATA.(['All',selectData])(:,i)),1/OFFDATA.LFPfs)...
                  +PNEtimeTemp(OFFDATA.(['All',selectData])(:,i))/(1/OFFDATA.LFPfs)))))';
@@ -154,37 +154,39 @@ try
 end    
     
 
-%Assess outliers
+% Store summary information
 allChannelstats=[];
 channelStatsFields=fields(OFFDATA.(['Stats',selectData]));
 channelStatsFields=string(channelStatsFields);
 for i = 1:length(channelStatsFields)
     allChannelstats(:,i)=OFFDATA.(['Stats',selectData]).(channelStatsFields(i));
 end
-%Find nan channels
+% Find empty channels to remove
 useChan=find(~isnan(allChannelstats(:,1)));
 
-if size(allChannelstats,2)<length(useChan)
-    for i = 1:size(allChannelstats,2)
+
+% Statistics 6: Mahalanobis distance
+% Calculate Mahalanobis distance for each channels as a measure of
+% divergence to help identify outlier channels (e.g. poor clustering)
+if length(useChan)>2
+    for i = 1:size(allChannelstats,2)-1
         mahalStore(:,i)=mahal(allChannelstats(useChan,i),allChannelstats(useChan,i));
     end
     OFFDATA.(['Stats',selectData]).MahalDist=nan(1,size(allChannelstats,1));
     OFFDATA.(['Stats',selectData]).MahalDist(useChan)=mean(mahalStore,2);
-    %OFFDATA.Stats.MahalDist=mahal(allChannelstats(:,1:5),allChannelstats(:,1:5));
 else
     OFFDATA.(['Stats',selectData]).MahalDist=ones(size(allChannelstats,1),1);
 end
-%end    
+    
 
-%% Draw figure
-%%%% Make button selection
+% Buttons to select channels statistic to view
 plotBG = uibuttongroup(g.Channelstats,...
     'Position',[0.75 0.2 0.22 0.6 ],'Visible','off',...
     'SelectionChangedFcn',@CHANNELSTAT_PLOT);
 
 uicontrol(plotBG,'Style',...
                   'radiobutton',...
-                  'String','Mahalanobis distance',...
+                  'String','OFF period duration',...
                   'Units','normalized',...
                   'Position',[0.1 0.75 1 0.2],...
                   'FontSize',13,...
@@ -192,7 +194,7 @@ uicontrol(plotBG,'Style',...
                   'HandleVisibility','off');
 uicontrol(plotBG,'Style',...
                   'radiobutton',...
-                  'String','Off period durations',...
+                  'String','OFF period number',...
                   'Units','normalized',...
                   'Position',[0.1 0.6 1 0.2],...
                   'FontSize',13,...
@@ -200,48 +202,49 @@ uicontrol(plotBG,'Style',...
                   'HandleVisibility','off');
 uicontrol(plotBG,'Style',...
                   'radiobutton',...
-                  'String','Off period number',...
+                  'String','OFF period occupancy',...
                   'Units','normalized',...
                   'Position',[0.1 0.45 1 0.2],...
                   'FontSize',13,...
                   'Tag','3',...
-                  'HandleVisibility','off');
-uicontrol(plotBG,'Style',...
-                  'radiobutton',...
-                  'String','Off period occupancy',...
-                  'Units','normalized',...
-                  'Position',[0.1 0.3 1 0.2],...
-                  'FontSize',13,...
-                  'Tag','4',...
                   'HandleVisibility','off');
 
 uicontrol(plotBG,'Style',...
                   'radiobutton',...
                   'String','Channel coherence',...
                   'Units','normalized',...
+                  'Position',[0.1 0.3 1 0.2],...
+                  'FontSize',13,...
+                  'Tag','4',...
+                  'HandleVisibility','off');   
+              
+uicontrol(plotBG,'Style',...
+                  'radiobutton',...
+                  'String','Mahalanobis distance',...
+                  'Units','normalized',...
                   'Position',[0.1 0.15 1 0.2],...
                   'FontSize',13,...
                   'Tag','5',...
-                  'HandleVisibility','off');    
+                  'HandleVisibility','off');
 
- if isempty(OFFDATA.LFPpathin)==0
+if isempty(OFFDATA.LFPpathin)==0
     uicontrol(plotBG,'Style',...
-    'radiobutton',...
-    'String','LFP amplitude',...
-    'Units','normalized',...
-    'Position',[0.1 0 1 0.2],...
-    'FontSize',13,...
-    'Tag','6',...
-    'HandleVisibility','off'); 
- end          
+                     'radiobutton',...
+                     'String','LFP amplitude',...
+                     'Units','normalized',...
+                     'Position',[0.1 0 1 0.2],...
+                     'FontSize',13,...
+                     'Tag','6',...
+                     'HandleVisibility','off'); 
+end          
               
 plotBG.Visible='on';
 
-%Clear temporary variables
+% Clear temporary variables
 clear PNEtimeTemp
 
 
-%%%% Plot default graph
+% Plot default graph (duration)
 plottingColors=[0    0.4470    0.7410
                        0.3010    0.7450    0.9330
                        0.4940    0.1840    0.5560
@@ -251,17 +254,15 @@ plottingColors=[0    0.4470    0.7410
                        0.9290    0.6940    0.1250];
 subplot('position',[0.07 0.12 0.65 0.8])
 set(gca,'ColorOrder',plottingColors)
-            gscatter([1:length(OFFDATA.Channels)],OFFDATA.(['Stats',selectData]).MahalDist,...
-                 [1:length(OFFDATA.Channels)],plottingColors,[],30,'off');
-            set(gca, 'XTick', 1:length(OFFDATA.Channels));
-            xlim([0 length(OFFDATA.Channels)+1])
-            set(gca, 'XTickLabels', OFFDATA.Channels)
-            ylabel('Mahalanobis distance');
+violinplot(OFFdurations,OFFdurationsID,...
+            'ShowData',false,'ViolinAlpha',0.3);
+            ylabel('Off period duration (ms)');
+            set(findobj('parent',gcf,'type', 'Axes'),'YScale','log')
             set(findobj('parent',gcf,'type', 'Axes'),'YTickLabel',get(findobj('parent',gcf,'type', 'Axes'),'YTick'))
-            
+            xlim([0 length(OFFDATA.Channels)+1])
 
     
-%%% Exit button
+% Exit button to return to main output page
 uicontrol(g.Channelstats,'Style', 'pushbutton','String','Done',...
     'FontWeight','bold','FontSize',12,...
     'BackgroundColor',[0.3 0.8 0.8],'Units','normalized',...
@@ -269,28 +270,21 @@ uicontrol(g.Channelstats,'Style', 'pushbutton','String','Done',...
     'set(findobj(''tag'',''OFFAD''),''Visible'',''on'');close(findobj(''tag'',''OFFAD_CHANNELSTATS''));');
 
     
+% Function to change statistic being viewed
 function CHANNELSTAT_PLOT(source,event)
        cla(findobj('parent',gcbf,'type', 'Axes'),'reset') 
        
-       if   event.NewValue.Tag=='1'
-            set(gca,'ColorOrder',plottingColors)
-            gscatter([1:length(OFFDATA.Channels)],OFFDATA.(['Stats',selectData]).MahalDist,...
-                 [1:length(OFFDATA.Channels)],plottingColors,[],30,'off');
-            set(gca, 'XTick', 1:length(OFFDATA.Channels));
-            xlim([0 length(OFFDATA.Channels)+1])
-            set(gca, 'XTickLabels', OFFDATA.Channels)
-            ylabel('Mahalanobis distance');
-            set(findobj('parent',gcf,'type', 'Axes'),'YTickLabel',get(findobj('parent',gcf,'type', 'Axes'),'YTick'))
-            
-       elseif event.NewValue.Tag=='2'
+       % Plot duration
+       if event.NewValue.Tag=='1'
             violinplot(OFFdurations,OFFdurationsID,...
             'ShowData',false,'ViolinAlpha',0.3);
             ylabel('Off period duration (ms)');
             set(findobj('parent',gcf,'type', 'Axes'),'YScale','log')
             set(findobj('parent',gcf,'type', 'Axes'),'YTickLabel',get(findobj('parent',gcf,'type', 'Axes'),'YTick'))
             xlim([0 length(OFFDATA.Channels)+1])
-            
-       elseif event.NewValue.Tag=='3'
+       
+       % Plot number
+       elseif event.NewValue.Tag=='2'
             set(gca,'ColorOrder',plottingColors)
             gscatter([1:length(OFFDATA.Channels)],OFFDATA.(['Stats',selectData]).OPnumber,...
                  [1:length(OFFDATA.Channels)],plottingColors,[],30,'off');
@@ -299,35 +293,42 @@ function CHANNELSTAT_PLOT(source,event)
             set(gca, 'XTickLabels', OFFDATA.Channels)
             ylabel('Off period number');
             set(findobj('parent',gcf,'type', 'Axes'),'YTickLabel',get(findobj('parent',gcf,'type', 'Axes'),'YTick'))
-
-       elseif event.NewValue.Tag=='4'
+        
+       % Plot occupancy time
+       elseif event.NewValue.Tag=='3'
             gscatter([1:length(OFFDATA.Channels)],OFFDATA.(['Stats',selectData]).OPoccupancy_time,...
                 [1:length(OFFDATA.Channels)],plottingColors,[],30,'off');
             set(gca, 'XTick', 1:length(OFFDATA.Channels));
             xlim([0 length(OFFDATA.Channels)+1])
             set(gca, 'XTickLabels', OFFDATA.Channels)
             ylabel('Off period occupancy (hours)');
-            
-       elseif event.NewValue.Tag=='5'
+        
+       % Plot coherence
+       elseif event.NewValue.Tag=='4'
             violinplot(coherence,coherenceID,...
             'ShowData',false,'ViolinAlpha',0.3);
              ylabel('Channel coherence score');
              xlim([0 length(OFFDATA.Channels)+1])
-             
-        elseif event.NewValue.Tag=='6'
-            %gscatter([1:length(OFFDATA.Channels)],OFFDATA.Stats.MeanLFPamp,...
-            %[1:length(OFFDATA.Channels)],plottingColors,[],30,'off');
-            %set(gca, 'XTick', 1:length(OFFDATA.Channels));
-            %xlim([0 length(OFFDATA.Channels)+1])
-            %set(gca, 'XTickLabels', OFFDATA.Channels)
-            %ylabel('LFP amplitude (uV)')
-            violinplot(LFPamp,LFPampID,...
+       
+       % Plot Mahalanobis distance
+       elseif   event.NewValue.Tag=='5'
+            set(gca,'ColorOrder',plottingColors)
+            gscatter([1:length(OFFDATA.Channels)],OFFDATA.(['Stats',selectData]).MahalDist,...
+                 [1:length(OFFDATA.Channels)],plottingColors,[],30,'off');
+            set(gca, 'XTick', 1:length(OFFDATA.Channels));
+            xlim([0 length(OFFDATA.Channels)+1])
+            set(gca, 'XTickLabels', OFFDATA.Channels)
+            ylabel('Mahalanobis distance');
+            set(findobj('parent',gcf,'type', 'Axes'),'YTickLabel',get(findobj('parent',gcf,'type', 'Axes'),'YTick'))
+       
+       % Plot LFP amplitude
+       elseif event.NewValue.Tag=='6'
+             violinplot(LFPamp,LFPampID,...
             'ShowData',false,'ViolinAlpha',0.3);
              ylabel('LFP amplitude(uV)');  
              xlim([0 length(OFFDATA.Channels)+1])
        end
-
-        
+       
 end
 
 end
