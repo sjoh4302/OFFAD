7e8b936ecab41d77af6c5864ac9404a0898db9cf
Christian Harding (laptop) <christian.harding@sjc.ox.ac.uk>
2022-09-15 11:28:50 +0100

    legacy files deleted

--- Untitled2.m
+++
@@ -1,9 +0,0 @@
-a=[];
-for i = 1:5
-try
-            GMModels = fitgmdist([1,2,3,4,5,6,7]',i,'Replicates',5);
-            a(:,i)=cluster(GMModels,[1,2,3,4,5,6,7]');
-        catch
-            return
-        end
-end
